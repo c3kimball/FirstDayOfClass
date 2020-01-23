47da0e5492d88361516948e7af35947465afb8f0
ckimb <c3kimball@student.bridgew.edu>
2020-01-23 12:01:28 -0500

    Simple Conversation added

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class DemoClass {
-    
+    public static void main(String[] args){
+        var reader = new Scanner(System.in);
+        System.out.print("What's your name: ");
+        var name = reader.nextLine();
+        System.out.println("Hello " + name);
+    }
 }
