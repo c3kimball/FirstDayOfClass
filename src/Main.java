fbb782320857246f5ac416b9150345c9d1ceb94d
ckimb <c3kimball@student.bridgew.edu>
2020-01-23 11:14:23 -0500

    Added an integer and for loop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args){
+        int x = 5;
         System.out.println("Hello World!");
         System.out.println("Lets make it work");
+        for(int i=0; i <= 5; i++){
+            System.out.println(x - i);
+        }
     }
 }
